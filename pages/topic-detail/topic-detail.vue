343a504987665726bdb5d64681c9419ac92c179e
PanYongcCheng <pyc1095937663@163.com>
2020-05-18 13:44:51 +0800

    上传项目

---
+++ pages/topic-detail/topic-detail.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<!-- 话题介绍 -->
+		<topicDetail :item="topinfo"></topicDetail>
+	</view>
+</template>
+
+<script>
+	import topicDetail from "../../components/topic/topic-detail.vue"; 
+	export default {
+		data() {
+			return {
+				topinfo:{
+					titlepic:"../../static/datapic/36.jpg",
+					titile:"标题标题",
+					desc:"描述描述",
+					totalnum:"200",
+					todaynum:"300"
+				}
+			}
+		},
+		methods: {
+			
+		},
+		components:{
+			topicDetail
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
